ce10ee81587783a6d6e12439b0b134b65a5a29a0
Rana Das <rana.pratap.das@gmail.com>
2019-03-10 07:43:45 +0000

    Adding (json for now ) data server application. This is used for testing from datagrid service

---
+++ datasetsetvice/src/main/java/com/rdas/datasets/config/JsonConfig.java
@@ -0,0 +1,31 @@
+package com.rdas.datasets.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+//https://www.stubbornjava.com/posts/practical-jackson-objectmapper-configuration
+@Order(1)
+@Configuration
+public class JsonConfig {
+
+    @Bean(name = "datasetJsonMapper")
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new JavaTimeModule());
+
+        //objectMapper.registerModule(new MetricsModule(TimeUnit.MINUTES, TimeUnit.MILLISECONDS, false));
+        return objectMapper;
+    }
+}
