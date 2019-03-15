5d718c233564adf5b0ed395950c35d374db7da02
Rana Das <rana.pratap.das@gmail.com>
2019-03-15 06:49:12 +0000

    adding a kt app for testing purpose only

---
+++ KTMain/src/main/kotlin/com/rdas/KTMain/KtMainApplication.kt
@@ -0,0 +1,11 @@
+package com.rdas.KTMain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtMainApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtMainApplication>(*args)
+}
---
+++ KTMain/src/test/kotlin/com/rdas/KTMain/KtMainApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.rdas.KTMain
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KtMainApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
